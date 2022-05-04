aff5bab4cc88d4cf48bbf3a720dea5cd84f82fe3
RuiqiWang <ruiqiwang1018@163.com>
2022-05-04 19:38:47 -0400

    update

--- ruiqi_wang_assignment2.js
+++ ruiqi_wang_assignment2.js
@@ -1,58 +1,89 @@
 const itemsObject = [
-    { quantity: 1, price: 200 },
-    { quantity: 3, price: 350 },
-    { quantity: 5, price: 400 },
+  { quantity: 1, price: 200 },
+  { quantity: 3, price: 350 },
+  { quantity: 5, price: 400 },
 ];
 
-// Q1
-function double(arr){
-    const newObj = [];
-    for(let i = 0; i < arr.length; i++){
-        newObj.push({quantity: arr[i].quantity * 2, price:arr[i].price * 2});
-    }
-    return newObj;
-    
+// Q1-1
+function double(arr) {
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push({ quantity: arr[i].quantity * 2, price: arr[i].price * 2 });
+  }
+  return newArr;
 }
-
+console.log("---Q1-1-----");
 console.log(double(itemsObject));
 
-//Q2
-function filterData(arr){
-    const newObj = []
-    l = arr.length;
-    for (let i = 0; i < l; i++){
-        if(arr[i].quantity > 2 && arr[i].price > 300 ){
-            newObj.push({quantity:arr[i].quantity,price:arr[i].price});
-            
-        }
-        
-    }
-    return newObj;
+//Q1 -2
+
+function double2(arr) {
+  return arr.map(doubleMap);
 }
 
+const doubleMap = ({ quantity, price }) => {
+  return { quantity: quantity * 2, price: price * 2 };
+};
+console.log("---Q1-2-----");
+console.log(double2(itemsObject));
+
+//Q2 - 1
+function filterData(arr) {
+  const newArr = [];
+  l = arr.length;
+  for (let i = 0; i < l; i++) {
+    if (arr[i].quantity > 2 && arr[i].price > 300) {
+      newArr.push({ quantity: arr[i].quantity, price: arr[i].price });
+    }
+  }
+  return newArr;
+}
+console.log("---Q2-1-----");
 console.log(filterData(itemsObject));
 
-//Q3
+//Q2 -2
+
+function filterData2(arr) {
+  return arr.filter(filterArray);
+}
+const filterArray = (obj) => {
+  return obj.quantity > 2 && obj.price > 300;
+};
+
+console.log("---Q2-2-----");
+console.log(filterData2(itemsObject));
+
+//Q3 - 1
 function calcu(arr) {
-    result = 0;
-    l = arr.length;
-    for (let i = 0; i < l; i++) {
-      result += arr[i].quantity * arr[i].price;
-    }
-    return result;
+  result = 0;
+  l = arr.length;
+  for (let i = 0; i < l; i++) {
+    result += arr[i].quantity * arr[i].price;
   }
-  
-  console.log(calcu(itemsObject));
+  return result;
+}
+console.log("---Q3-1-----");
+console.log(calcu(itemsObject));
+
+//Q3-2
+function calculate(arr) {
+  let total = 0;
+  return arr.reduce(function(total, obj){
+    return total + obj.quantity * obj.price;
+  },total);
+
+}
+console.log("---Q3-2-----");
+console.log(calculate(itemsObject));
 
 //Q4
 const string =
   " Perhaps The Easiest-to-understand   Case   For Reduce Is   To Return The Sum Of  All The Elements In  An Array  ";
 
 function arrange(str) {
-  const regex = /[^A-Za-z]/g;
-  const newStr = str.replace(regex, "");
-  return newStr.toLowerCase();
+
+  const newStr = str.toLowerCase().trim().split(/[ -]+/).join(" ");
+  return newStr;
 }
-
+console.log("---Q4-----");
 console.log(arrange(string));
-
